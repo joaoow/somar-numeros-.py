5a9281386ec489894803b30d9d7f8e07ce852919
joaolinux <87071803+joaolinux@users.noreply.github.com>
2022-05-25 15:00:10 -0300

    soma.py
    soma de números inteiros

---
+++ soma.py
@@ -0,0 +1,7 @@
+# Utilize o int antes do input para números inteiros
+# caso ao contrário quando vc digitar um número ex: 10 + 5 o código vai somar esse formato 105
+n1 = int (input('Digite um número:'))
+n2 = int (input('Digite mais um número'))
+s = n1 + n2
+print('A soma é', s)
+
